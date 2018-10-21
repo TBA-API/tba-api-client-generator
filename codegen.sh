a9c3e6e05a9064fd6a1a557e763ad14b24519392
Travis Weir <tweirtx@gmail.com>
2018-10-21 17:00:57 -0500

    Update codegen.sh

--- codegen.sh
+++ codegen.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env sh
 if [ "$OUTLANG" = "swift" ] ; then
-  export GENLANG="swift4"
+  export $GENLANG="swift4"
 else:
-  export GENLANG="$OUTLANG"
+  export $GENLANG="$OUTLANG"
 fi
 export OPTIONS="-i $SPEC_FILE -l $GENLANG -o $REPO_NAME -c configs/$OUTLANG-config.json"
 
