bd7bda67bacb1d6ac1b4d5398fe8ca18b8f03b54
Travis Weir <tweirtx@gmail.com>
2019-05-06 13:37:07 -0500

    Try to skip spec validation

--- codegen.sh
+++ codegen.sh
@@ -4,7 +4,7 @@ if [ "$OUTLANG" = "swift" ] ; then
 else
   export GENLANG="$OUTLANG"
 fi
-export OPTIONS="-i $SPEC_FILE -g $GENLANG -o $REPO_NAME -c configs/$OUTLANG-config.json"
+export OPTIONS="-i $SPEC_FILE -g $GENLANG -o $REPO_NAME -c configs/$OUTLANG-config.json" --skip-validate-spec
 
 
 #if [ "$OUTLANG" = "python" ] ; then
