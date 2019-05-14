6874556fad4401cb05b01d6ed0b41c7a87ee39fd
Travis Weir <tweirtx@gmail.com>
2019-05-13 21:07:01 -0500

    Forget that

--- codegen.sh
+++ codegen.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
-if [[ "$OUTLANG"=="swift" ]]; then
-    export GENLANG="swift4"
-else
+#if [[ "$OUTLANG"=="swift" ]]; then
+    # export GENLANG="swift4"
+#else
     export GENLANG="$OUTLANG"
-fi
+#fi
 export OPTIONS="-i $SPEC_FILE -g $GENLANG -o $REPO_NAME -c configs/$OUTLANG-config.json --skip-validate-spec"
 
 
