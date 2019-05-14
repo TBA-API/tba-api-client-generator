b80a224f7eba1168d51d74b68a28f2dd2b940a79
Travis Weir <tweirtx@gmail.com>
2019-05-13 22:00:50 -0500

    We don't need no billion ifs

--- codegen.sh
+++ codegen.sh
@@ -1,23 +1,15 @@
 #! /usr/bin/env bash
-#if [[ "$OUTLANG"=="swift" ]]; then
-    # export GENLANG="swift4"
-#else
+
+if [[ "$OUTLANG"=="swift" ]]; then
+    export GENLANG="swift4"
+else
     export GENLANG="$OUTLANG"
-#fi
+fi
+
 export OPTIONS="-i $SPEC_FILE -g $GENLANG -o $REPO_NAME -c configs/$OUTLANG-config.json --skip-validate-spec"
 
 
-if [[ "$OUTLANG"==="python" || "$OUTLANG"==="csharp" || "$OUTLANG"==="go" ]] ; then
-  export OPTIONS="$OPTIONS --additional-properties packageVersion=$APIVERSION"
-elif [[ "$OUTLANG"==="javascript" ]]; then
-  export OPTIONS="$OPTIONS --project-version $APIVERSION"
-elif [[ "$OUTLANG"==="swift" ]]; then
-  export OPTIONS="$OPTIONS --pod-version $APIVERSION"
-elif [[ "$OUTLANG"==="ruby" ]]; then
-    export OPTIONS="$OPTIONS --gem-version $APIVERSION"
-else
-  export OPTIONS="$OPTIONS --artifact-version $APIVERSION"
-fi
+export OPTIONS="$OPTIONS --additional-properties packageVersion=$APIVERSION"
 echo $OPTIONS
 echo $APIVERSION
 openapi-generator generate $OPTIONS
